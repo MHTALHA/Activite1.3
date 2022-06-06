8057916ebabd3ed436479c7994ce378c37df52c6
Mohamed Hossein Talha <mhtalha@dom.tti>
2022-06-06 15:22:18 +0100

    updated save data to articles.json

--- posterArticle.php
+++ posterArticle.php
@@ -23,58 +23,59 @@
 
 <?php
 
-array();
-$arr_data[] = array(); // create empty array
+//array();
+$articles[] = array(); // create empty array
 
 $myFile = "articles.json";
 
 if(isset($_POST["submit"])){
 if((isset($_POST["titre"])) && (isset($_POST["auteur"])) && (isset($_POST["date"]) )&& (isset($_POST["date"])) && (isset($_POST["description"]))){
     if(!empty($_POST["titre"]) && !empty($_POST["auteur"]) && !empty($_POST["date"])&& !empty($_POST["date"]) && !empty($_POST["description"])){
+        try
+        {
+          //Get form data
+          $formdata = array(
+           'id'=>$_REQUEST['id'],
+           'titre'=> htmlspecialchars($_POST['titre']), 
+           'auteur' => htmlspecialchars($_POST['auteur']),
+           'date'=> $_POST['date'], 
+           'description'=> strip_tags($_POST['description']) 
+           
+          );
+       
+       
+         //Get data from existing json file
+          $jsondata = file_get_contents($myFile);
+       //print_r($jsondata)."<br/>";
+          // converts json data into array
+          $articles = json_decode($jsondata, true);
+        //print_r($articles)."<br/>";
+          // Push article data to array
+          array_push($articles,$formdata);
+       
+          //Convert updated array to JSON
+          $jsondata = json_encode($articles, JSON_PRETTY_PRINT);
+       
+          //write json data into data.json file
+          if(file_put_contents($myFile, $jsondata)) {
+           echo "<div class='alert alert-success' role='alert'>
+           JSON file a été créée avec succée...!
+         </div>";
+         print_r($jsondata);
+           }
+          else 
+               echo "error";
+       
+          }
+       
+           catch (Exception $e) {
+               echo 'Caught exception: ',  $e->getMessage(), "\n";
+         }
         
     }
 }
 };
 
-try
- {
-   //Get form data
-   $formdata = array(
-    'id'=>$_REQUEST['id'],
-    'titre'=> htmlspecialchars($_POST['titre']), 
-    'auteur' => htmlspecialchars($_POST['auteur']),
-    'date'=> $_POST['date'], 
-    'description'=> strip_tags($_POST['description']) 
-    
-   );
-
-
-  //Get data from existing json file
-   $jsondata = file_get_contents($myFile);
-
-   // converts json data into array
-   $arr_data = json_decode($jsondata, true);
-
-   // Push article data to array
-   array_push($arr_data,$formdata);
-
-   //Convert updated array to JSON
-   $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
-
-   //write json data into data.json file
-   if(file_put_contents($myFile, $jsondata)) {
-    echo "<div class='alert alert-success' role='alert'>
-    JSON file a été créée avec succée...!
-  </div>";
-    }
-   else 
-        echo "error";
-
-   }
-
-    catch (Exception $e) {
-        echo 'Caught exception: ',  $e->getMessage(), "\n";
-  }
 
 
    ?>
@@ -84,26 +85,26 @@ try
                     <div class="form-group row">
                         <label for="titre" class="col-md-2 col-form-label">Titre</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" id="titre" name="titre" placeholder="taper le titre">
+                            <input type="text" class="form-control"  name="titre" placeholder="taper le titre">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="auteur" class="col-md-2 col-form-label">Auteur</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" id="auteur" name="auteur" placeholder="taper le nom d'auteur">
+                            <input type="text" class="form-control"  name="auteur" placeholder="taper le nom d'auteur">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="date" class="col-12 col-md-2 col-form-label">Date de publication</label>
                         <div class="col-5 col-md-3">
-                            <input type="date" class="form-control" id="date" name="date">
+                            <input type="date" class="form-control"  name="date">
                         </div>
                     </div>
                    
                     <div class="form-group row">
                         <label for="description" class="col-md-2 col-form-label">Description</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" id="description" name="description" placeholder="" rows="12"></textarea>
+                            <textarea class="form-control"  name="description" placeholder="" rows="12"></textarea>
                         </div>
                     </div>
                     <div class="form-group row">
